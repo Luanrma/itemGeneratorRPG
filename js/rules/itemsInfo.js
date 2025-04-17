75fb4b07a307cc6f398248533725c165c789d9e5
Luan Raisson Miano de Aquino <42654434+Luanrma@users.noreply.github.com>
2025-04-17 12:17:27 -0300

    Update itemsInfo.js

--- js/rules/itemsInfo.js
+++ js/rules/itemsInfo.js
@@ -14,5 +14,32 @@ export default {
         orb: 4,
         brics: 5,
         special_item: 6
+    },
+
+    armor_parts: {
+        helm: 1,
+        chest: 2,
+        gloves: 3,
+        pants: 4,
+        boots: 5,
+    },
+
+    weapon_types: {
+        two_hands_sword: 1,        // Espada 2M
+        one_hand_sword: 2,         // Espada 1M
+        two_hands_axe: 3,          // Machado 2M
+        two_hands_spear: 4,        // Lança 2M
+        two_hands_bow: 5,          // Arco 2M
+        one_hand_dagger: 6,        // Adaga 1M
+        one_hand_knife: 7,         // Faca 1M
+        one_hand_staff: 8,         // Cajado 1M
+        two_hands_staff: 9,        // Cajado 2M
+        two_hands_firearm: 10,     // Arma de Fogo 2M
+        one_hand_firearm: 11,      // Arma de Fogo 1M
+        one_hand_hammer: 12,       // Martelo 1M
+        one_hand_club: 13,         // Clava 1M
+        two_hands_hammer: 14,      // Martelo 2M
+        two_hands_club: 15,        // Clava 2M
+        one_hand_spellbook: 16     // Livro de Feitiços 1M
     }
 }
